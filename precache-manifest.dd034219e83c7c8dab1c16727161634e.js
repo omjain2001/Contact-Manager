772b06842f1133a93d9f355a881382a186b5eac8
Om Jain <omjain2606@gmail.com>
2020-09-06 00:24:31 +0530

    Updates

---
+++ precache-manifest.dd034219e83c7c8dab1c16727161634e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03a656a42efe57e2719144dd8428142",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "d10be0019f5b1b659135",
+    "url": "/Contact-Manager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d2735adbed0dce620fd1",
+    "url": "/Contact-Manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d10be0019f5b1b659135",
+    "url": "/Contact-Manager/static/js/2.3a6188ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Contact-Manager/static/js/2.3a6188ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2735adbed0dce620fd1",
+    "url": "/Contact-Manager/static/js/main.868857ea.chunk.js"
+  },
+  {
+    "revision": "f73ffd0cfb4889d60082",
+    "url": "/Contact-Manager/static/js/runtime-main.481eb384.js"
+  }
+]);
